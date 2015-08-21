08cfa8980044f0819bb4e256441f129a7ae31300
Byeol <byeol@hotmail.co.kr>
2015-08-21 21:15:05 +0900

    Fix bugs of private messaging plugin Display attachments in past messages

--- wp-content/plugins/private-messaging/app/views/shortcode/_inbox_message.php
+++ wp-content/plugins/private-messaging/app/views/shortcode/_inbox_message.php
@@ -94,7 +94,8 @@ if (!isset($render_reply)) {
                         <div class="col-md-6 message-attachment">
                             <a class="load-attachment-info" data-target="#<?php echo $id ?>" href="#<?php echo $id ?>">
                                 <i class="fa fa-paperclip fa-2x pull-left"></i>
-                                test.png </a>
+                                <?php echo $a_m->name ?>
+                            </a>
 
                             <div class="clearfix"></div>
                             <!-- Modal -->
@@ -214,6 +215,107 @@ if (!isset($render_reply)) {
                     <div class="message-body">
                         <?php echo mmg()->html_beautifier($message->content) ?>
                     </div>
+                    <?php $ids = explode(',', $message->attachment);
+                    $ids = array_filter($ids);
+                    if (count($ids)):?>
+                        <hr/>
+                        <div class="message-footer">
+                            <div class="row">
+                                <?php foreach ($ids as $id): ?>
+                                    <?php $a_m = IG_Uploader_Model::model()->find($id); ?>
+                                    <div class="col-md-6 message-attachment">
+                                        <a class="load-attachment-info" data-target="#<?php echo $id ?>" href="#<?php echo $id ?>">
+                                            <i class="fa fa-paperclip fa-2x pull-left"></i>
+                                            <?php echo $a_m->name ?>
+                                        </a>
+
+                                        <div class="clearfix"></div>
+                                        <!-- Modal -->
+                                        <div class="modal" id="<?php echo $id ?>">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h4 class="modal-title"><?php echo $a_m->name ?></h4>
+                                                    </div>
+                                                    <div class="modal-body sample-pop" style="max-height:450px;overflow-y:scroll">
+                                                        <?php
+                                                        $file = $a_m->file;
+                                                        //check does this file exist
+
+                                                        $file_url = '';
+                                                        $show_image = false;
+
+                                                        if ($file) {
+                                                            $file_url = wp_get_attachment_url($file);
+                                                            $mime = explode('/', get_post_mime_type($file));
+                                                            if (array_shift($mime) == 'image') {
+                                                                $show_image = true;
+                                                            }
+                                                        }
+                                                        if ($show_image) {
+                                                            echo '<img src="' . $file_url . '"/>';
+                                                        } elseif ($file) {
+                                                            //show meta
+                                                            ?>
+                                                            <ul class="list-group">
+                                                                <li class="list-group-item upload-item">
+                                                                    <i class="glyphicon glyphicon-floppy-disk"></i>
+                                                                    <?php _e('Size', mmg()->domain) ?>:
+                                                                    <strong><?php
+                                                                        $tfile = get_attached_file($file);
+                                                                        //check does this files has deleted
+                                                                        if ($tfile) {
+                                                                            $size = @filesize($tfile);
+                                                                            echo $size === false ? 'N/A' : $size;
+                                                                        } else {
+                                                                            echo __("N/A", mmg()->domain);
+                                                                        }
+                                                                        ?></strong>
+                                                                </li>
+                                                                <li class="list-group-item upload-item">
+                                                                    <i class="glyphicon glyphicon-file"></i>
+                                                                    <?php _e('Type', mmg()->domain) ?>:
+                                                                    <strong><?php echo ucwords(get_post_mime_type($file)) ?></strong>
+                                                                </li>
+                                                            </ul>
+                                                        <?php
+                                                        } else {
+                                                            ?>
+                                                            <ul class="list-group">
+                                                                <li class="list-group-item">
+                                                                    <i class="glyphicon glyphicon-link"></i>
+                                                                    <strong><?php _e('Link', mmg()->domain) ?></strong>:
+                                                                    <?php echo $a_m->url ?>
+                                                                </li>
+                                                                <div class="clearfix"></div>
+                                                            </ul>
+                                                        <?php
+                                                        } ?>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <?php if ($a_m->url): ?>
+                                                            <a class="btn btn-info" rel="nofollow"
+                                                               href="<?php echo esc_attr($a_m->url) ?>" target="_blank">
+                                                                <?php _e("Visit Link", mmg()->domain) ?>
+                                                            </a>
+                                                        <?php endif; ?>
+                                                        <?php if ($a_m->file): ?>
+                                                            <a href="<?php echo $file_url ?>" download
+                                                               class="btn btn-info"><?php _e('Download File', mmg()->domain) ?></a>
+                                                        <?php endif; ?>
+                                                        <button type="button" class="btn btn-default attachment-close"
+                                                                data-dismiss="modal">Close
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                                <div class="clearfix"></div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
 
                 </section>
             <?php endforeach; ?>
